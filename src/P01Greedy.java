e08b0234e3235bf438e016252296a7bb35ee1d66
kadun1 <kadunjj@gmail.com>
2022-03-22 23:03:06 +0900

    프로그래머스 코딩테스트 탐욕1완료

--- src/P01Greedy.java
+++ src/P01Greedy.java
@@ -1,5 +1,60 @@
 public class P01Greedy {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int n = 3;
+        int[] lost = {3};
+        int[] reserve = {1};
+        int solution = solution(n, lost, reserve);
+        System.out.println(solution);
+    }
+
+    static int solution(int n, int[] lost, int[] reserve){
+        int answer = 0;
+
+        int[] members = new int[n];
+
+        for (int i = 0; i < members.length; i++) {
+            members[i] = 1;
+        }
+
+        for (int i = 0; i < reserve.length; i++) {
+            members[reserve[i]-1] += 1;
+        }
+
+        for (int i = 0; i < lost.length; i++) {
+            members[lost[i]-1] -= 1;
+        }
+
+        for (int i = 0; i < members.length; i++) {
+            if(i==0){
+                if(members[i]==0&&members[i+1]==2){
+                    members[i]+=1;
+                    members[i+1]-=1;
+                }
+            } else if(i!=members.length-1){
+                if(members[i]==0&&members[i-1]==2) {
+                    members[i] += 1;
+                    members[i - 1] -= 1;
+                } else if(members[i]==0&&members[i+1]==2){
+                    members[i]+=1;
+                    members[i+1]-=1;
+                } else if(members[i]==0&&members[i-1]==2){
+                    members[i]+=1;
+                    members[i-1]-=1;
+                }
+            } else {
+                if(members[i]==0&&members[i-1]==2){
+                    members[i]+=1;
+                    members[i-1]-=1;
+                }
+            }
+        }
+
+        for (int i = 0; i < members.length; i++) {
+            if(members[i]>0){
+                answer++;
+            }
+        }
+        return answer;
     }
 }
