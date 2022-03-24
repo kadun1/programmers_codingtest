8e6460611dc2c4b0cc9ff28a4d97bf005ca88e50
kadun1 <kadunjj@gmail.com>
2022-03-24 21:28:48 +0900

    프로그래머스 코딩테스트 해시지만 sort로 풀어버림

--- src/P02Hash.java
+++ src/P02Hash.java
@@ -1,2 +1,21 @@
+import java.util.Arrays;
+
 public class P02Hash {
+
+    public static void main(String[] args) {
+        String[] pb = {"1211","97674223","111", "1195524421"};
+        boolean answer = solution(pb);
+        System.out.println(answer);
+    }
+
+    static boolean solution(String[] phone_book){
+
+        Arrays.sort(phone_book);
+        for (int i = 1; i < phone_book.length; i++) {
+            if(phone_book[i].startsWith(phone_book[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
 }
