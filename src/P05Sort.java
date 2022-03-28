59032f1b3a20adc7ad04ba87350d21ce89cdb909
kadun1 <kadunjj@gmail.com>
2022-03-29 00:41:17 +0900

    프로그래머스 코딩테스트 정렬 진행중...

--- src/P05Sort.java
+++ src/P05Sort.java
@@ -1,12 +1,40 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+
 public class P05Sort {
     public static void main(String[] args) {
-        int[] numbers = {6, 10, 2};
+        int[] numbers = {6,10,2};
         solution(numbers);
     }
 
     static String solution(int[] numbers){
         String answer = "";
+        List<String> list = new ArrayList<>();
 
+        Comparator<String> stringComparator = new Comparator<>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return 0;
+            }
+        };
+
+        String a = "30";
+        String b = "3";
+        if(a.hashCode()>b.hashCode()){
+            System.out.println(a.hashCode());
+        }
+
+        stringComparator.compare(answer, answer);
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            list.add(numbers[i]+"");
+        }
+        for (String s: list) {
+            answer += s;
+        }
         return answer;
     }
 }
