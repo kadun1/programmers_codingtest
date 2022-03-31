35a5c851fbb8aeb46171aba169e0bf3104a12f30
kadun1 <kadunjj@gmail.com>
2022-03-31 21:23:44 +0900

    프로그래머스 코딩테스트 카카오 기초문제(보물지도)

---
+++ src/P06Bit.java
@@ -0,0 +1,17 @@
+public class P06Bit {
+
+    public static void main(String[] args) {
+        int[] arr1 = {9, 20, 28, 18, 11};
+        int[] arr2 = {30, 1, 21, 17, 28};
+        String[] arrStr = new String[5];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arrStr[i] = Integer.toBinaryString(arr1[i]|arr2[i]).replace("1", "#").replace("0", " ");
+        }
+
+        for (String s: arrStr
+             ) {
+            System.out.println(s);
+        }
+    }
+}
