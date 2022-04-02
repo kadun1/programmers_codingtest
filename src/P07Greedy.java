0fabf5a125e2a51d9dad2f2d47d0f3b66bda1560
kadun1 <kadunjj@gmail.com>
2022-04-02 18:46:45 +0900

    프로그래머스 코딩테스트 탐욕 : 당장은 알고리즘을 구현하지 못함.. 로직을 이해하자

--- src/P07Greedy.java
+++ src/P07Greedy.java
@@ -1,19 +1,30 @@
 public class P07Greedy {
 
     public static void main(String[] args) {
-        int answer = solution("JEROENZA");
+        int answer = solution("AAABBBA");
         System.out.println("answer = " + answer);
     }
 
     public static int solution(String name){
         int answer = 0;
         int len = name.length();
+        int move = len - 1;
 
         for (int i = 0; i < name.length(); i++) {
+            //이 부분이 결국 이동의 핵심로직
             answer += Math.min(name.charAt(i)-'A', 'Z'-name.charAt(i)+1);
 
-//            A의 갯수에 따라.. 되돌아가는 것이 빠를수 있음을 구현해야함
+            //이 아래가...
+            int nextint = i + 1;
+            while(nextint < len && name.charAt(nextint)=='A'){
+                nextint++;
+            }
+
+            //이해가 안가네...
+            move = Math.min(move, i * 2 + len - nextint);
+
+            move = Math.min(move, (len - nextint) * 2 + i);
         }
-        return answer;
+        return answer + move;
     }
 }
