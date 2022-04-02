3a5e40bb349abf382962ccfbcbe3159cf0102fad
kadun1 <kadunjj@gmail.com>
2022-04-03 00:04:13 +0900

    프로그래머스 코딩테스트 스택,큐.. 이것도 로직을 이해하자

---
+++ src/P08Queue.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class P08Queue {
+
+    public static void main(String[] args) {
+
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+        int answer = solution(priorities, location);
+        System.out.println(answer);
+
+    }
+
+    //위치와 값을 담는 클래스
+    public static class MyTest {
+        int location;
+        int priority;
+        public MyTest(int location, int priority) {
+            this.location = location;
+            this.priority = priority;
+        }
+    }
+
+    static int solution(int[] priorities, int location) {
+        int answer = 0;
+
+        Queue<MyTest> queue = new LinkedList<>();
+
+        for (int i = 0; i < priorities.length; i++) {
+            queue.add(new MyTest(i, priorities[i]));
+        }
+
+        int index = 0;
+        while (!queue.isEmpty()) {
+            boolean flag = false;
+            MyTest pq = queue.poll();
+            for (MyTest t : queue) {
+                if (t.priority > pq.priority) {
+                    flag = true;
+                }
+            }
+            if(flag){
+                queue.add(pq);
+            } else {
+                index++;
+                if (pq.location == location) {
+                    answer = index;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
